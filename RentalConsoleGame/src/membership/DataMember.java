46cb5044412d2d43bba0f2073673e537c3b5cf0a
paul-lestyo <lestyo24@gmail.com>
2021-11-22 12:39:25 +0700

    add project LKPD 12

---
+++ RentalConsoleGame/src/membership/DataMember.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package membership;
+
+/**
+ *
+ * @author WIN10
+ */
+public class DataMember {
+    String ID;
+    String Nama;
+    public String Jenis;
+    
+    public DataMember(String ID, String Nama, String Jenis) {
+        this.ID = ID;
+        this.Nama = Nama;
+        this.Jenis = Jenis;
+    }
+    
+    void print() {
+        System.out.println("ID Member       : " + this.ID);
+        System.out.println("Nama Member     : " + this.Nama);
+        System.out.println("Jenis Member    : " + this.Jenis);
+        System.out.println();
+    }
+}
